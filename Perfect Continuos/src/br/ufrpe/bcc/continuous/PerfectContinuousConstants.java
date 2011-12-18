b00c5253b164edc2de11630b9fd132fdb7a50cee
sergiolisan <sergiolisan@4477a642-f698-0fa3-91af-41c58435787b>
2011-12-18 20:48:45 +0000

    Estruturacao definitiva do iBusiness

---
+++ Perfect Continuos/src/br/ufrpe/bcc/continuous/PerfectContinuousConstants.java
@@ -0,0 +1,14 @@
+package br.ufrpe.bcc.continuous;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author serginho
+ */
+public interface PerfectContinuousConstants {
+    
+    Color bright_bcc = new Color(220,230,240);
+    
+    
+}
