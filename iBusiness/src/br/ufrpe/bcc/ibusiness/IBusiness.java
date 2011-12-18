d8a6cbeba7e60cbfdbf0e254d07050ec2848e973
sergiolisan <sergiolisan@4477a642-f698-0fa3-91af-41c58435787b>
2011-12-18 19:44:37 +0000

    Estruturacao definitiva do iBusiness

--- iBusiness/src/br/ufrpe/bcc/ibusiness/IBusiness.java
+++ iBusiness/src/br/ufrpe/bcc/ibusiness/IBusiness.java
@@ -1,5 +1,9 @@
 package br.ufrpe.bcc.ibusiness;
 
+import br.ufrpe.bcc.ibusiness.ui.IBusinessUI;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Sergio
@@ -10,6 +14,13 @@ public class IBusiness {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EventQueue.invokeLater( new Runnable() {
+
+            @Override
+            public void run() {
+                JFrame ui = new IBusinessUI();
+                ui.setVisible(true);
+            }
+        });
     }
 }
